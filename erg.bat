8bcafb8cc05f551a7024fb51488f1a9a50336506
xdeep0 <77788695+xdeep0@users.noreply.github.com>
2021-10-12 01:05:21 +0900

    Update erg.bat

--- erg.bat
+++ erg.bat
@@ -1,13 +1,13 @@
 @echo off
 
-curl -OL https://raw.githubusercontent.com/xdeep0/a/main/7z_aria2.zip
+curl -OL https://raw.githubusercontent.com/xdeep0/a/main/7z_aria.tgz
 tar xf 7z_aria.tgz
 
 rem download gigafile
 set gf_url=%1
 for /f "usebackq delims=" %%a in (`curl -w "%%{redirect_url}" -s -o nul %gf_url%`) do set gf_url=%%a
 curl -s -c cookie -o nul %gf_url%
-aria2c -q -k1M -s16 -x16 -o erg.7z --load-cookies=cookie %gf_url:~0,23%download.php?file=%gf_url:~23%
+aria2c -k1M -s16 -x16 -o erg.7z --load-cookies=cookie %gf_url:~0,23%download.php?file=%gf_url:~23%
 
 7z x -p%2 erg.7z
 
