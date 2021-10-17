fba442b941156eba884d498fb14a829b80f58aa0
xdeep0 <77788695+xdeep0@users.noreply.github.com>
2021-10-17 20:14:27 +0900

    Update erg.bat

--- erg.bat
+++ erg.bat
@@ -1,29 +1,30 @@
 @echo off
 
-set username=Administrator
+set g_utils=%1
+set g_url=%2
+set g_pswd=%3
+set g_save_name=%4
+set g_save_dir=%5
+set g_save_parent=%6
+set g_work_dir=C:\Users\Administrator\Downloads
+set path=%path%;%g_work_dir%\bin
 
-rem download vb_cable
-curl -L -o vb.zip https://download.vb-audio.com/Download_CABLE/VBCABLE_Driver_Pack43.zip
-mkdir vb
-tar xf vb.zip -C vb
-del vb.zip
+curl -L -o utils.tgz %g_utils%
+tar xf utils.tgz
+del utils.tgz
 
-rem download directx, chrome rdp
-curl -OL https://download.microsoft.com/download/1/7/1/1718CCC4-6315-4D8E-9543-8E28A4E18C4C/dxwebsetup.exe
-curl -OL https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi
+for /f "usebackq delims=" %%a in (`curl -w "%%{redirect_url}" -s -o nul %g_url%`) do set g_url=%%a
+curl -s -c c.txt -o nul %g_url%
+aria2c -k1M -s16 -x16 -o g.7z --load-cookies=c.txt %gf_url:~0,23%download.php?file=%gf_url:~23%
+7z x -p%g_pswd% g.7z
+del c.txt g.7z
 
-curl -OL https://raw.githubusercontent.com/xdeep0/a/main/7z_aria.tgz
-tar xf 7z_aria.tgz
-del 7z_aria.tgz
+rclone copy dr:%g_save_name% .
+tar xf %g_save_name% -C %g_save_dir%
+del %g_save_name%
 
-rem download gigafile
-set gf_url=%1
-for /f "usebackq delims=" %%a in (`curl -w "%%{redirect_url}" -s -o nul %gf_url%`) do set gf_url=%%a
-curl -s -c cookie -o nul %gf_url%
-aria2c -k1M -s16 -x16 -o erg.7z --load-cookies=cookie %gf_url:~0,23%download.php?file=%gf_url:~23%
-
-7z x -p%2 erg.7z
-
-tar xf C:\Users\%username%\Desktop\sv_tmfr2.tgz -C C:\Users\%username%\AppData\Roaming
-
-del 7z_aria.zip cookie erg.7z
+(
+echo @echo off
+echo tar caf %g_save_name% -C %g_save_dir% %g_save_parent%
+echo rclone copy %g_save_name% dr:
+) > save.bat
